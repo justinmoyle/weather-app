3df1e2c092ce1df6de073744431e9480a68202a5
justinmoyle <195149107+justinmoyle@users.noreply.github.com>
2025-11-17 17:28:54 -0500

    dynamically pull from weather api to push to page

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=imperial&q
 
 const userInput = document.getElementById('city-search');
 const submitBtn = document.getElementById('search-btn');
+const weatherIcon = document.getElementById('temp-img');
 
 async function checkWeather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -15,7 +16,15 @@ async function checkWeather(city) {
     document.querySelector('#humidity-percent').innerHTML = data.main.humidity + '%';
     document.querySelector('#windspeed-amount').innerHTML = Math.round(data.wind.speed) + ' m/hr';
 
-
+    if (data.weather[0].main == 'Clouds') {
+        weatherIcon.src = 'assets/cloudy.png';
+    } else if (data.weather[0].main == 'Clear') {
+        weatherIcon.src = 'assets/sunny.png';
+    } else if (data.weather[0].main == 'Precipitation') {
+        weatherIcon.src = 'assets/raining.png';
+    } else if (data.weather[0].main == 'Severe Weather') {
+        weatherIcon.src = 'assets/storm.png';
+    }
 }
 
 submitBtn.addEventListener('click', () => {
